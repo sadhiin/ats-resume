00e3e1117ae5b8c6751d9cee50acdb916c050019
Shanjidul Islam Sadhin <66504343+sadhiin@users.noreply.github.com>
2025-12-02 11:58:22 +0600

    feat: Implement initial ATS resume optimization crew with file loading, task definitions, and configuration.

---
+++ task.py
@@ -0,0 +1,55 @@
+from crewai import Task
+
+def parse_resume_task(agent, raw_resume_text):
+    
+    truncated_resume_text = raw_resume_text[:1000] if len(raw_resume_text) > 1000 else raw_resume_text
+    return Task(
+        agent=agent,
+        description=(
+            f"Clear the following text quickly: {truncated_resume_text}\n\n"
+            "Remove artifacts, normalized formatting, bulet points to '-' keep all the content be fast and direct"
+        ),
+        expected_output_type="Clear resume text with proper structure"
+    )
+
+
+def rewrite_for_ats_task(agent, cleaned_resume_text, job_description, job_title):
+    truncated_resume_text = cleaned_resume_text[:1000] +"..." if len(cleaned_resume_text) > 1000 else cleaned_resume_text
+    truncated_job_description = job_description[:1000] +"..." if len(job_description) > 1000 else job_description
+    return Task(
+        agent=agent,
+        description=(
+            f"Rewrite the following resume for job {job_title} with job description \n\n{truncated_job_description}\n\n"
+            f"Resume: {truncated_resume_text}\n\n"
+            "Rewrite the resume text for ATS",
+            "Match the keywords use action verbs add metircs, Terget 80+ ATS score. Be direct and fast"
+        ),
+        expected_output_type="Ats optimized resume with keyword placement and metrics"
+    )
+
+
+def refine_bulet_task(agent, rewritten_resume_text):
+    truncated_resume_text = rewritten_resume_text[:1000] +"..." if len(rewritten_resume_text) > 1000 else rewritten_resume_text
+    return Task(
+        agent=agent,
+        description=(
+            f"Plished those bulet points with action verbs and metrics: \n\n{truncated_resume_text}\n\n"
+            "Add strong verbs and numbers. Be direct and fast"
+        ),
+        expected_output_type="Resume with enhanced bullet points and metrics"
+    )
+
+def evaluation_ats_task(agent, final_resume_text, job_title, job_description):
+    truncated_resume_text = final_resume_text[:1000] +"..." if len(final_resume_text) > 1000 else final_resume_text
+    truncated_job_description = job_description[:1000] +"..." if len(job_description) > 1000 else job_description
+
+    return Task(
+        agent=agent,
+        description=(
+            f"Evaluate and score the resume for job {job_title} with job description \n\n{truncated_job_description}\n\n"
+            f"Resume: {truncated_resume_text}\n\n"
+            "Evaluate the resume text for ATS and provide the score",
+            "Rate 1-5: keywords, structure, metrics, verbs, format. Return Json with overall score (0-100) breakdown of the gaps"
+        ),
+        expected_output_type="ATS score and recommendations"
+    )
